2cd03a58b00ca4d2d8c94b66e353ec9c59cd7591
cribba1 <4CRIBA61@solent.ac.uk>
2019-10-01 09:39:27 +0000

    testing saving

---
+++ mess.py
@@ -0,0 +1,13 @@
+number1 = 0
+number2 = 0
+
+number1 = int(input("test text"))
+number2 = int(input("test text2"))
+
+
+if number1 > number2:
+    print("no1>no2")
+elif number1 == number2:
+    print("they are the same")
+else:
+    print("no2 > no1")
